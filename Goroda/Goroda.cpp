5d0ab25eaf3b91d981ac860dc514fcdb02fd4b14
Colledge <Colledge@DESKTOP-CBRV7QC>
2024-10-01 15:28:26 +0900

    Добавьте файлы проекта.

---
+++ Goroda/Goroda.cpp
@@ -0,0 +1,38 @@
+﻿using namespace std;
+#include <iostream>
+
+
+int main()
+{
+    float a, b, res;
+    cout << "enter first dsget: \n";
+    cin >> a;
+    cout << "enter second deget: \n";
+    cin >> b;
+    //реализация меню
+    char key;
+    cout << "\n + see Sum \n";
+    //..............
+    //ввод оператора
+    cin >> key;
+    //реализация swich
+    switch (key)
+    {
+    case '+':
+        res = a + b;
+        cout << res;
+        break;
+    case '-':
+        res = a - b;
+        cout << res;
+        break;
+    case '*':
+        res = a * b;
+        cout << res;
+        break;
+    case '/':
+        res = a / b;
+        cout << res;
+        break;
+    }
+}
